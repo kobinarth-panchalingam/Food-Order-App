761540538e70253ee249196019139439141e9584
kobinarth22@gmail.com <kobinarth22@gmail.com>
2023-05-26 11:40:31 +0530

    logout button added

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,18 +1,31 @@
 import { Nav, Navbar } from "react-bootstrap";
 import "../styles/style.css";
 import { Link } from "react-router-dom";
+import { FiLogOut } from "react-icons/fi";
+import LocalStorageService from "../utils/LocalStorageService";
 
 function NavBar({ activeTab }) {
   const handleTabSelect = (tab) => {
     activeTab = tab;
   };
 
+  const handleLogout = () => {
+    // Delete the user object from LocalStorageService
+    LocalStorageService.removeItem("user");
+
+    // Perform any additional logout logic here
+
+    // Redirect to the login or home page
+    // Example: window.location.href = "/login";
+    window.location.href = "/";
+  };
+
   return (
     <>
       <Navbar bg="light" sticky="top">
-        <Navbar.Brand className="mx-2" href="#">
+        {/* <Navbar.Brand className="mx-2" href="#">
           Pick Me
-        </Navbar.Brand>
+        </Navbar.Brand> */}
         <Nav fill variant="tabs" activeKey={activeTab} onSelect={handleTabSelect}>
           <Nav.Link as={Link} to="/foodMenu" eventKey="tab1">
             Order Here
@@ -25,6 +38,9 @@ function NavBar({ activeTab }) {
           </Nav.Link>
           <Nav.Link as={Link} to="/pastOrders " eventKey="tab4">
             Past Orders
+          </Nav.Link>
+          <Nav.Link onClick={handleLogout}>
+            <FiLogOut />
           </Nav.Link>
         </Nav>
       </Navbar>
